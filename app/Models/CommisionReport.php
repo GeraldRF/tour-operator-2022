32ce6873b563ad06921eb3887f8d13d051a898f1
Gerald Ramírez <gerald.rafo@gmail.com>
2022-07-08 09:58:09 -0600

    Modelos Bill,  BillReport, CommisionReport, ConfirmationEmailSent, ServiceReport; sus factories y migrations

---
+++ app/Models/CommisionReport.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CommisionReport extends Model
+{
+    use HasFactory;
+}
