bfee1ceb23489cf3a0eefcbb5e6fe74e2457d5dd
Gerald Ramírez <gerald.rafo@gmail.com>
2022-07-16 22:49:34 -0600

    use tailwind cdn

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,13 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/css/app.css">
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>Inicio | {{str_replace('_', ' ', env('APP_NAME'))}}</title>
 </head>
 <body>
   <h1 class="bg-red-400">Welcome</h1> 
   
   
-  <script src="https://unpkg.com/flowbite@1.4.7/dist/flowbite.js"></script>
 </body>
 </html>
