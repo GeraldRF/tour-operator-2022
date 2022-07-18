6dd2fefc2980a1a40e9c8a5f52af6a2b711b7614
Jafher08 <jafhernandez08@gmail.com>
2022-07-17 22:54:50 -0600

    Test Driver almacenar

--- tests/Feature/DriverTest.php
+++ tests/Feature/DriverTest.php
@@ -25,6 +25,66 @@ class DriverTest extends TestCase
 
     }
 
-    
+    /** @test */
+    public function el_sistema_puede_editar_un_conductor_de_la_db()
+    {
+        //crear conductor
+        $driver = Driver::factory()->create([
+            'nombre' => 'Juan',
+            'cedula' => '123456789',
+            'fecha_nacimiento' => '2020-07-08',
+            'tipo_licencia' => 'A',
+        ]);
+
+        $response = $this->get('chofer/'.$driver->id.'/edit');
+
+       $response->assertStatus(200)->assertSee($driver->nombre);
+
+        //editar conductor
+        $driver->nombre = 'Alan';
+        $driver->cedula = '987654321';
+        $driver->fecha_nacimiento = '2020-07-09';
+        $driver->tipo_licencia = 'B';
+     
+        $this->put('chofer/'.$driver->id, $driver->toArray());
+
+        $response->assertSessionHas('success_msg', 'Actualizado correctamente');
+
+        $response = $this->get('chofer');
+
+        $response->assertStatus(200)->assertSee($driver->nombre);
+
+    }
+
+    /** @test */
+    public function el_sistema_puede_eliminar_un_conductor()
+    {
+        //crear conductor
+        $driver = Driver::factory()->create([
+            'nombre' => 'Juan',
+            'cedula' => '123456789',
+            'fecha_nacimiento' => '2020-07-08',
+            'tipo_licencia' => 'A',
+        ]);
+        //eliminar conductor
+        $driver->delete();
+        //verificar que el conductor se elimino correctamente
+        $this->assertNull($driver->fresh());
+    }
+
+    /** @test */
+    //mostrar conductor en consola
+    public function el_sistema_puede_mostrar_datos_de_un_conductor()
+    {
+        //crear conductor
+        $driver = Driver::factory()->create([
+            'nombre' => 'Juan',
+            'cedula' => '123456789',
+            'fecha_nacimiento' => '2020-07-08',
+            'tipo_licencia' => 'A',
+        ]);
+        //mostrar datos del conductor
+        $this->assertDatabaseHas('drivers', ['nombre' => 'Juan']);
+    }
 
 }
